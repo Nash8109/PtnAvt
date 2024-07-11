41076501abfacd1bcfaf246cffdd95966f2eba5a
Natalya Shabalina <n7500794@gmail.com>
2024-07-11 18:15:32 +0400

    add seventh homework

---
+++ Lesson_7/constants.py
@@ -0,0 +1,6 @@
+# Первое задание
+Test_from_URL = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+# Второе Задание
+Test_from_URL =  "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+# Третье задание
+Test_from_URL = "https://www.saucedemo.com/"
